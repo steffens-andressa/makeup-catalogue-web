62a93d370320b69dd67bd94804f477146e829846
andressa-steffens <steffensand@hotmail.com>
2022-07-12 04:09:13 -0300

    :sparkles: Create component <footer> and enable materialize on Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  @ViewChild('mobile') sideNav?: ElementRef; 
+  
   title = 'makeup-catalogue-web';
+
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sideNav?.nativeElement);
+  } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListComponent } from './list/list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListComponent
+    ListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
